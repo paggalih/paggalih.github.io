90390409cdf2c24b76530c4d7cb5a62b0cb60d3e
paggalih <67624808+paggalih@users.noreply.github.com>
2021-05-28 21:27:45 +0700

    a

--- js/umum.js
+++ js/umum.js
@@ -1,16 +1,32 @@
 // FUNGSI READ MORE
-function RM() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("RMBtn");
+// function RM() {
+//     var dots = document.getElementById("dots1");
+//     var moreText = document.getElementById("more");
+//     var btnText = document.getElementById("RMBtn");
   
+//     if (dots.style.display === "none") {
+//       dots.style.display = "inline";
+//       btnText.innerHTML = "Perlihatkan"; 
+//       moreText.style.display = "none";
+//     } else {
+//       dots.style.display = "none";
+//       btnText.innerHTML = "Sembunyikan"; 
+//       moreText.style.display = "inline";
+//     }
+//   }
+
+function RM(x) {
+    let dots = document.querySelector(`.card[data-x="${x}"] .dots`);
+    let moreText = document.querySelector(`.card[data-x="${x}"] .more`); 
+    let btnText = document.querySelector(`.card[data-x="${x}"] .RMBtn`);
+
     if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Perlihatkan"; 
-      moreText.style.display = "none";
+        dots.style.display = "inline";
+        btnText.textContent = "Perlihatkan";
+        moreText.style.display = "none";
     } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Sembunyikan"; 
-      moreText.style.display = "inline";
+        dots.style.display = "none";
+        btnText.textContent = "---Sembunyikan---"; 
+        moreText.style.display = "inline";
     }
-  }
+}
