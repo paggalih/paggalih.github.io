094cfe72f20b5eecd949f3f4c006528aa932adec
paggalih <67624808+paggalih@users.noreply.github.com>
2024-06-04 10:23:43 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -362,3 +362,22 @@ loadStylesheets(stylesheets);
 const html = document.querySelector('html')
 // html.setAttribute("dir", "rtl");
 html.data-bs-theme = "auto";
+
+/**
+ * Author and copyright: Stefan Haack (https://shaack.com)
+ * Repository: https://github.com/shaack/bootstrap-auto-dark-mode
+ * License: MIT, see file 'LICENSE'
+ */
+
+;(function () {
+    const htmlElement = document.querySelector("html")
+    if(htmlElement.getAttribute("data-bs-theme") === 'auto') {
+        function updateTheme() {
+            document.querySelector("html").setAttribute("data-bs-theme",
+                window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light")
+        }
+
+        window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', updateTheme)
+        updateTheme()
+    }
+})()
