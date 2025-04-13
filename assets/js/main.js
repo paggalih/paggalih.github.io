be607295f8c6715e3f2f2a4172eda93bf5fe681b
paggalih <67624808+paggalih@users.noreply.github.com>
2025-04-13 10:33:26 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -335,7 +335,7 @@ container.appendChild(socialLinks);
     var head = document.getElementsByTagName('head')[0];
     var script = document.createElement('script');
     script.type = 'text/javascript';
-    script.src = "http://code.jquery.com/jquery-2.2.1.min.js";
+    script.src = "https://code.jquery.com/jquery-2.2.1.min.js";
 
     // Then bind the event to the callback function.
     // There are several events for cross browser compatibility.
